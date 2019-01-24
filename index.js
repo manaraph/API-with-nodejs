577dcc0216a1e17a17d0c270a67a0de70e461bca
Manasseh Omachonu <manaraph225@gmail.com>
2019-01-24 19:50:56 +0100

    Install and mongoose and connect to mongoDB database

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const hapi = require('hapi');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://admin:GijhSH3eR9XTp7k@ds211635.mlab.com:11635/node_api'); //This is just a DB
+
+mongoose.connection.once('open', ()=>{
+    console.log('Connected to database');
+    
+})
+
 
 const server = hapi.server({
     port: 4000,
