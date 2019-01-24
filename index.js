5b2ae7b71954e71469a50b0acc70c91ffe316c03
Manasseh Omachonu <manaraph225@gmail.com>
2019-01-24 20:22:02 +0100

    Added more routes

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
 const hapi = require('hapi');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://admin:GijhSH3eR9XTp7k@ds211635.mlab.com:11635/node_api'); //This is just a DB
+//Set up hapi server
+const server = hapi.server({
+    port: 4000,
+    host: 'localhost'
+});
+
+
+//This credential is not correct create your database on mlab and connect to yours
+// mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds357261.mlab.com:<yourport>/<yourdb>'); 
+mongoose.connect('mongodb://admin:GijhSH3eR9XTp7k@ds211635.mlab.com:11635/node_api'); 
 
 mongoose.connection.once('open', ()=>{
     console.log('Connected to database');
     
 })
 
-
-const server = hapi.server({
-    port: 4000,
-    host: 'localhost'
-});
-
+const Painting = require('./models/Painting')
+//Set up routes
 const init = async () => {
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: function(request, reply){
-            return `<h1>My modern api</h1?`
+    server.route([
+        {
+            method: 'GET',
+            path: '/',
+            handler: function(request, reply){
+                return `<h1>My modern api</h1?`
+            }
+        },
+        {
+            method: 'GET',
+            path: '/api/v1/paintings',
+            handler: (req, reply)=>{
+                return Painting.find();
+            }
+        },
+        {
+            method: 'POST',
+            path: '/api/v1/paintings',
+            handler: (req, reply)=>{
+                const{name, url, techniques} = req.payload;
+                const painting = new Painting({
+                    name,
+                    url,
+                    techniques
+                })
+                return Painting.save();
+            }
         }
-    });
+    ]);
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
     
