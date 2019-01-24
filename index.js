3eac549573bb87acc606aa33306c3ceb2e6a3efe
Manasseh Omachonu <manaraph225@gmail.com>
2019-01-24 08:55:33 +0100

    Install hapijs, setup server and connection, set up routing

---
+++ index.js
@@ -0,0 +1,21 @@
+const hapi = require('hapi');
+
+const server = hapi.server({
+    port: 4000,
+    host: 'localhost'
+});
+
+const init = async () => {
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function(request, reply){
+            return `<h1>My modern api</h1?`
+        }
+    });
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+    
+};
+
+init();
