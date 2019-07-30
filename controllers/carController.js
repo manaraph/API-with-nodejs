b122c6746c75e8cf702e892863291c783acda6e0
Manasseh Omachonu <manaraph225@gmail.com>
2019-07-30 17:11:26 +0100

    models and controllers

---
+++ controllers/carController.js
@@ -0,0 +1,52 @@
+const boom = require('boom');
+const Car = require('../models/Car');
+
+exports.getCars = async (req, res) => {
+  try {
+    const cars = await Car.find();
+    return cars;
+  } catch (error) {
+    throw boom.boomify(error);
+  }
+};
+
+exports.getSingleCar = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const car = await Car.findById(id);
+    return car;
+  } catch (error) {
+    throw boom.boomify(error);
+  }
+};
+
+exports.addCar = async (req, res) => {
+  try {
+    const car = new Car(req.body);
+    return car.save();
+  } catch (error) {
+    throw boom.boomify(error);
+  }
+};
+
+exports.updateCar = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const car = req.body;
+    const { ...updateData } = car;
+    const update = await Car.findByIdAndUpdate(id, updateData, { new: true });
+    return update;
+  } catch (error) {
+    throw boom.boomify(error);
+  }
+};
+
+exports.deleteCar = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const car = await Car.findByIdAndRemove(id);
+    return car;
+  } catch (error) {
+    throw boom.boomify(error);
+  }
+};
