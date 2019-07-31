dcce740f7ff6bfdd97ad4eac8f94447ffd0f81b3
Manasseh Omachonu <manaraph225@gmail.com>
2019-07-31 03:45:18 +0100

    add seed script, add option for decaprecation warning in mongo db

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fastlify = require('fastify')({ logger: true});
 import mongoose, { mongo } from 'mongoose';
 
 // Connect to mongoDB
-mongoose.connect('mongodb://localhost/mygarage')
+mongoose.connect('mongodb://localhost/mygarage', { useNewUrlParser: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
